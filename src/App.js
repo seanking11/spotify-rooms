edf85f8de71e71eba4cd344cc761a69ece040259
seanking11 <seanking@Seans-MacBook-Pro.local>
2017-06-17 17:31:02 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton'
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+        <RaisedButton label="Hello" secondary />
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;

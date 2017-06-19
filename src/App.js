054ccb0d2dd4b820942bd34af501a1b4279bf489
seanking11 <seanking@Seans-MacBook-Pro.local>
2017-06-18 21:41:14 -0500

    Add connection to API

--- src/App.js
+++ src/App.js
@@ -7,7 +7,44 @@ import RaisedButton from 'material-ui/RaisedButton'
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
+const url = 'https://api.spotify.com/v1/artists/0OdUWJ0sBjDrqHygGUXeCF'
+
 class App extends Component {
+  constructor(props){
+      super(props)
+      this.state = {
+        requestFailed: false,
+        dataSlice: {}
+      }
+    }
+
+  componentDidMount() {
+      fetch("https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg", {
+        header: {
+          Accept: "application/json",
+          Authorization: "Bearer BQBI520u0D8DAQBaQeEbW-clBJgGYjr1a3n_pArB5KhpGt16ZA217YqRa0_iBoS3vViz5mLq7oz9y5YPMiIAjKI4UdxXGqqYGmKILt-GPsDvsOQM_C5qce2usfpXNR09gltEfACiSOWsfZmhKg"
+        }
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw Error("Network request failed")
+        }
+
+        return response;
+      })
+      .then(d => d.json())
+      .then(d => {
+        console.log(d);
+        this.setState({
+          apiData: d
+        })
+      }, () => {
+        this.setState({
+          requestFailed: true
+        })
+      })
+    }
+
   render() {
     return (
       <MuiThemeProvider>
